64350848c85b7cf30e732dea002ad704f76637ae
bingoohuang <bingoo.huang@gmail.com>
2024-09-03 12:38:18 +0800

    ss. SplitReg

--- ss/splitx.go
+++ ss/splitx.go
@@ -1,6 +1,7 @@
 package ss
 
 import (
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
@@ -103,4 +104,58 @@ func tryAddPart(subs []string, sub string) []string {
 	}
 
 	return subs
+}
+
+// SplitReg slices s into substrings separated by the expression and returns a slice of
+// the substrings between those expression matches. (especially including the tail separator.)
+//
+// The slice returned by this method consists of all the substrings of s
+// not contained in the slice returned by FindAllString. When called on an expression
+// that contains no metacharacters, it is equivalent to strings.SplitN.
+//
+// Example:
+//
+//	s := regexp.MustCompile("a*").SplitReg("abaabaccadaaae", 5)
+//	// s: ["", "b", "b", "c", "cadaaae"]
+//
+// The count determines the number of substrings to return:
+//
+//	n > 0: at most n substrings; the last substring will be the unsplit remainder.
+//	n == 0: the result is nil (zero substrings)
+//	n < 0: all substrings
+func SplitReg(s string, re *regexp.Regexp, n int) []string {
+	if n == 0 {
+		return nil
+	}
+
+	if len(re.String()) > 0 && len(s) == 0 {
+		return []string{""}
+	}
+
+	matches := re.FindAllStringIndex(s, n)
+	result := make([]string, 0, len(matches))
+
+	beg, end := 0, 0
+	for _, match := range matches {
+		if n > 0 && len(result) >= n-1 {
+			break
+		}
+
+		end = match[0]
+		if match[1] != 0 {
+			if sub := strings.TrimSpace(s[beg:end]); sub != "" {
+				// including the seps
+				result = append(result, sub+s[match[0]:match[1]])
+			}
+		}
+		beg = match[1]
+	}
+
+	if end != len(s) {
+		if sub := strings.TrimSpace(s[beg:]); sub != "" {
+			result = append(result, sub)
+		}
+	}
+
+	return result
 }
