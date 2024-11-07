cc7e972c45387cec46d23d9bec9d548a38e611b8
huangjinbing <bingoo.huang@gmail.com>
2024-11-07 21:15:25 +0800

    StructEnv

---
+++ ss/struct.go
@@ -0,0 +1,176 @@
+package ss
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+// StructEnv 通过对应的环境变量的值，设置结构体字段的值.
+// 结构体字段支持以下类型或者该类型的指针:
+// string, bool,
+// uint64, uint32, uint16, uint8, uint,
+// int64, int32, int16, int8, int,
+// float32, float64
+// time.Duration
+// 可以通过在 Tag 中使用 env:"-" 忽略某个字段。或者 env:"ENV_NAME" 来指定环境变量名。
+// 如果所设置的环境变量值类型不符合，将会返回错误
+// 参数 v 必须是结构体变量的指针。
+// 示例:
+//
+//	type Config struct {
+//		Port int
+//	}
+//	func main() {
+//		var cfg Config
+//		StructEnv(&cfg, &StructEnvOptions{Prefix: "MYAPP_", PanicOnError: true})
+//		// 环境变量 export MYAPP_PORT = 10000
+//		fmt.Println(cfg.Port) // 会打印 10000
+//	}
+func StructEnv(v any, envOptions ...*StructEnvOptions) error {
+	if len(envOptions) == 0 {
+		envOptions = []*StructEnvOptions{{}}
+	}
+
+	opt := envOptions[0]
+	err := structEnv(v, opt)
+	if err != nil && opt.PanicOnError {
+		panic(err)
+	}
+	return err
+}
+
+func structEnv(v any, opt *StructEnvOptions) error {
+
+	reflectVal := reflect.ValueOf(v)
+	if reflectVal.Kind() != reflect.Ptr {
+		return fmt.Errorf("v must be pointer")
+	}
+
+	valueElem := reflectVal.Elem()
+	for i := 0; i < valueElem.NumField(); i++ {
+		structField := valueElem.Type().Field(i)
+		if !structField.IsExported() {
+			continue
+		}
+
+		envName := structField.Tag.Get("env")
+		if envName == "-" {
+			continue
+		}
+		if envName == "" {
+			envName = ToSnakeUpper(structField.Name)
+		}
+
+		fieldType := structField.Type
+		kind := fieldType.Kind()
+		isPtr := kind == reflect.Ptr
+		if isPtr {
+			fieldType = fieldType.Elem()
+			kind = fieldType.Kind()
+		}
+
+		envValue := os.Getenv(opt.Prefix + envName)
+		if envValue == "" {
+			verbose := false
+			if fieldType == timeDurationType {
+				verbose = true
+			} else {
+				switch kind {
+				case reflect.String, reflect.Bool,
+					reflect.Uint64, reflect.Uint32, reflect.Uint16, reflect.Uint8, reflect.Uint,
+					reflect.Int64, reflect.Int32, reflect.Int16, reflect.Int8, reflect.Int,
+					reflect.Float32, reflect.Float64:
+					verbose = true
+				default:
+					// ignore other types
+				}
+			}
+
+			if verbose && Pick1(ParseBool(os.Getenv("STRUCT_ENV_VERBOSE"))) {
+				log.Printf("env %s can be set as %s", envName, kind)
+			}
+
+			continue
+		}
+
+		field := valueElem.Field(i)
+		if fieldType == timeDurationType {
+			d, _, err := ParseDur(envValue)
+			if err != nil {
+				return fmt.Errorf("parse %s = %s as Duration error: %v", envName, envValue, err)
+			}
+
+			if isPtr {
+				field.Set(reflect.ValueOf(&d))
+			} else {
+				field.Set(reflect.ValueOf(d))
+			}
+			continue
+		}
+
+		switch kind {
+		case reflect.String:
+			if isPtr {
+				field.Set(reflect.ValueOf(&envValue))
+			} else {
+				field.SetString(envValue)
+			}
+		case reflect.Bool:
+			ev, err := ParseBool(envValue)
+			if err != nil {
+				return fmt.Errorf("parse %s = %s as Bool error: %v", envName, envValue, err)
+			}
+			if isPtr {
+				field.Set(reflect.ValueOf(&ev))
+			} else {
+				field.SetBool(ev)
+			}
+		case reflect.Uint64, reflect.Uint32, reflect.Uint16, reflect.Uint8, reflect.Uint:
+			ev, err := strconv.ParseUint(envValue, 10, 64)
+			if err != nil {
+				return fmt.Errorf("parse %s = %s as Uint64 error: %v", envName, envValue, err)
+			}
+			if isPtr {
+				field.Set(reflect.ValueOf(&ev))
+			} else {
+				field.SetUint(ev)
+			}
+		case reflect.Int64, reflect.Int32, reflect.Int16, reflect.Int8, reflect.Int:
+			ev, err := strconv.ParseInt(envValue, 10, 64)
+			if err != nil {
+				return fmt.Errorf("parse %s = %s as Int64 error: %v", envName, envValue, err)
+			}
+			if isPtr {
+				field.Set(reflect.ValueOf(&ev))
+			} else {
+				field.SetInt(ev)
+			}
+
+		case reflect.Float32, reflect.Float64:
+			ev, err := strconv.ParseFloat(envValue, 64)
+			if err != nil {
+				return fmt.Errorf("parse %s = %s as Float64 error: %v", envName, envValue, err)
+			}
+			if isPtr {
+				field.Set(reflect.ValueOf(&ev))
+			} else {
+				field.SetFloat(ev)
+			}
+		default:
+			// ignore other types
+		}
+	}
+
+	return nil
+}
+
+// StructEnvOptions is the options for StructEnv.
+type StructEnvOptions struct {
+	// Prefix is the prefix of environment variable name.
+	Prefix string
+	// PanicOnError will panic if there is an error.
+	PanicOnError bool
+}
---
+++ ss/struct_test.go
@@ -0,0 +1,73 @@
+package ss
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestStructEnv(t *testing.T) {
+	// 设置环境变量
+	os.Setenv("STRUCT_ENV_VERBOSE", "1")
+	os.Setenv("TEST_STRING", "test_string")
+	os.Setenv("TEST_DURATION", "1s")
+	os.Setenv("TEST_UINT", "123")
+	os.Setenv("TEST_INT", "-456")
+	os.Setenv("TEST_BOOL", "1")
+	os.Setenv("TEST_FLOAT", "78.9")
+
+	// 确保在测试结束时清除环境变量
+	defer os.Unsetenv("STRUCT_ENV_VERBOSE")
+	defer os.Unsetenv("TEST_STRING")
+	defer os.Unsetenv("TEST_DURATION")
+	defer os.Unsetenv("TEST_UINT")
+	defer os.Unsetenv("TEST_INT")
+	defer os.Unsetenv("TEST_BOOL")
+	defer os.Unsetenv("TEST_FLOAT")
+
+	// 创建一个测试结构体实例
+	type TestStruct struct {
+		String   *string        `env:"TEST_STRING"`
+		Uint     *uint64        `env:"TEST_UINT"`
+		Duration *time.Duration `env:"TEST_DURATION"`
+		Int      *int64         `env:"TEST_INT"`
+		Bool     *bool          `env:"TEST_BOOL"`
+		Float    *float64       `env:"TEST_FLOAT"`
+
+		TestString   string
+		TestString2  string
+		TestDuration time.Duration
+		TestUint     uint64
+		TestInt      int64
+		TestBool     bool
+		TestFloat    float64
+
+		TestFloatIgnore float64 `env:"-"`
+
+		Other time.Time
+		name  string
+	}
+
+	testStruct := TestStruct{}
+
+	// 调用 StructEnv 函数
+	StructEnv(&testStruct)
+
+	// 断言环境变量被正确解析
+	assert.Equal(t, "test_string", *testStruct.String)
+	assert.Equal(t, "test_string", testStruct.TestString)
+	assert.Equal(t, time.Second, *testStruct.Duration)
+	assert.Equal(t, time.Second, testStruct.TestDuration)
+	assert.Equal(t, uint64(123), *testStruct.Uint)
+	assert.Equal(t, uint64(123), testStruct.TestUint)
+	assert.Equal(t, int64(-456), *testStruct.Int)
+	assert.Equal(t, int64(-456), testStruct.TestInt)
+	assert.Equal(t, true, *testStruct.Bool)
+	assert.Equal(t, true, testStruct.TestBool)
+	assert.Equal(t, 78.9, *testStruct.Float)
+	assert.Equal(t, 78.9, testStruct.TestFloat)
+	assert.Equal(t, 0.0, testStruct.TestFloatIgnore)
+}
+
+// 如果你的结构体字段使用了非导出的名称，或者有 "-" 标签，或者有自定义的类型，你需要添加额外的测试用例来处理这些情况。
